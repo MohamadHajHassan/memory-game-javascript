3f5d2d2c887ef3d071585c67a2e49b190fefb5ab
Mohamad Haj Hassan <mohamadhajhassan33@gmail.com>
2022-09-16 15:12:50 +0300

    add assign cards fct

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,14 @@
 window.onload = () => {
   // Variables
-  const cardsArray = [1, 1, 2, 2, 3, 3];
-
+  const backDiv = document.querySelectorAll(".back");
+  const cardsArray = [
+    `<img src="images/html-logo.png" alt="" />`,
+    `<img src="images/html-logo.png" alt="" />`,
+    `<img src="images/css-logo.png" alt="" />`,
+    `<img src="images/css-logo.png" alt="" />`,
+    `<img src="images/js-logo.png" alt="" />`,
+    `<img src="images/js-logo.png" alt="" />`,
+  ];
   //   Functions
   const shuffle = array => {
     let currentIndex = array.length;
@@ -15,4 +22,13 @@ window.onload = () => {
       ];
     }
   };
+
+  const assignCards = (array1, array2) => {
+    for (let i = 0; i < 6; i++) {
+      array1[i].innerHTML = array2[i];
+    }
+  };
+
+  shuffle(cardsArray);
+  assignCards(backDiv, cardsArray);
 };
