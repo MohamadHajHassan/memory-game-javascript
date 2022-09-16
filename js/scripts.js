f4972a090b5b7587e00736ef96278eced740183e
Mohamad Haj Hassan <mohamadhajhassan33@gmail.com>
2022-09-16 14:28:04 +0300

    add shuffle fct

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
 window.onload = () => {
-  console.log("Hello");
+  // Variables
+  const cardsArray = [1, 1, 2, 2, 3, 3];
+
+  //   Functions
+  const shuffle = array => {
+    let currentIndex = array.length;
+    let randomIndex;
+    while (currentIndex > 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ];
+    }
+  };
 };
